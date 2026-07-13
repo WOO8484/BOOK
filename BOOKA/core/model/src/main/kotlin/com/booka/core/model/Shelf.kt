8e0ea4dc1e9492863b610a760aaa0041f4c3b863
WOO8484 <qudrnr84@gmail.com>
2026-07-13 14:06:05 +0900

    Delete BOOKA directory

--- BOOKA/core/model/src/main/kotlin/com/booka/core/model/Shelf.kt
+++
@@ -1,8 +0,0 @@
-package com.booka.core.model
-
-data class Shelf(
-    val id: String,
-    val name: String,
-    val workIds: List<String>,
-    val sortOrder: Int,
-)
--- BOOKA/feature/metadata/src/main/kotlin/com/booka/feature/metadata/MetadataAnalysisContract.kt
+++
@@ -1,7 +0,0 @@
-package com.booka.feature.metadata
-
-sealed interface MetadataAnalysisUiState {
-    data object Analyzing : MetadataAnalysisUiState
-    data object Done : MetadataAnalysisUiState
-    data class Failed(val message: String) : MetadataAnalysisUiState
-}
